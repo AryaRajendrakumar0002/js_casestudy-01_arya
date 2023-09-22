843b17b91ae812f4634d2e6de84873c0f7013e4e
Arya <mailtoaryaraj@gmail.com>
2023-09-22 12:22:39 +0530

    latest

--- code3.js
+++ code3.js
@@ -1,1 +1,11 @@
-hi
+for (let i=0; i<=15; i++) {
+    if (i === 0) {
+            console.log(i +  " is an even number");
+    }
+    else if (i % 2 === 0) {
+            console.log(i + " is an even number");   
+    }
+    else {
+            console.log(i + " is an odd number");
+    }
+}
