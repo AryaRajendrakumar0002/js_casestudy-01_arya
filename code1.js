ea7d98173e0055d4d4e69e1fdbaf1b684756783d
Arya <mailtoaryaraj@gmail.com>
2023-09-22 09:11:28 +0530

    new

--- code1.js
+++ code1.js
@@ -1,4 +1,18 @@
 var arr =[1,2,3,4];
 function array(){
-    
+    console.log(arr[0]);
 }
+array();
+let n =arr[0];
+function prime(n){
+    
+    if(n<2)
+    return '${n}not a prime number'
+      for(let i =2;i<n;i++){
+          if(n % i === 0){
+              return '${n}not a prime number'
+          }
+      }
+      return '${n} is a prime number'
+}
+prime(n);
